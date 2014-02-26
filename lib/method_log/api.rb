684179ff760951fb28a1d426e1159d95d25ab8df
James Mead <james@floehopper.org>
2014-02-26 16:24:41 +0000

    Find only first definition of method in commit.
    This is an optimisation. Given that we were only considering the first
    definition of the method, there's no need to keep looking for others
    once we've found one.

--- lib/method_log/api.rb
+++ lib/method_log/api.rb
@@ -15,11 +15,10 @@ module MethodLog
         @repository.commits(max_count: max_count).each do |commit|
           next if last_source_file && commit.contains?(last_source_file)
           source_files = commit.source_files.select { |sf| sf.source[Regexp.new(method_name)] }
-          method_definitions = source_files.inject([]) do |definitions, source_file|
-            method_finder = MethodFinder.new(source_file: source_file)
-            definitions += Array(method_finder.find(method_identifier))
+          method_definition = nil
+          source_files.map { |sf| MethodFinder.new(source_file: sf) }.each do |method_finder|
+            break if method_definition = method_finder.find(method_identifier)
           end
-          method_definition = method_definitions.first
           last_source_file = method_definition && method_definition.source_file
           yielder << MethodCommit.new(commit: commit, method_definition: method_definition)
         end

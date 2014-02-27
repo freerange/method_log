8db99fbeb58b293798870837a0c252708003dea3
James Mead <james@floehopper.org>
2014-02-27 16:43:14 +0000

    Inline local variables and make higher-level assertions.
    This makes the test more readable and more end-to-end.

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -21,40 +21,27 @@ module MethodLog
     end
 
     it 'finds class instance method in repository with two commits with single source file' do
-      foo_1 = source(path: 'foo.rb', source: %{
+      repository = Repository.new(path: repository_path)
+      commit_1 = repository.commit(source(path: 'foo.rb', source: %{
         class Foo
           def bar
             # implementation 1
           end
         end
-      })
-
-      foo_2 = source(path: 'foo.rb', source: %{
+      }))
+      commit_2 = repository.commit(source(path: 'foo.rb', source: %{
         # move method definition down one line
         class Foo
           def bar
             # implementation 2
           end
         end
-      })
+      }))
 
-      repository = Repository.new(path: repository_path)
-      commit_1 = repository.commit(foo_1)
-      commit_2 = repository.commit(foo_2)
-
-      repository = Repository.new(path: repository_path)
-      api = API.new(repository: repository)
-      method_commits = api.history('Foo#bar').to_a
-
-      method_definition_1 = MethodDefinition.new(source_file: foo_1, lines: 1..3)
-      method_definition_2 = MethodDefinition.new(source_file: foo_2, lines: 2..4)
-
-      method_commit_1 = MethodCommit.new(commit: commit_1, method_definition: method_definition_1)
-      method_commit_2 = MethodCommit.new(commit: commit_2, method_definition: method_definition_2)
-
-      expect(method_commits).to eq([method_commit_2, method_commit_1])
-
+      api = API.new(repository: Repository.new(path: repository_path))
       method_commit, method_diff = api.diffs('Foo#bar').first
+
+      expect(method_commit.sha).to eq(commit_2.sha)
       expect(method_diff.to_s.chomp).to eq(unindent(%{
            def bar
         -    # implementation 1
@@ -64,24 +51,23 @@ module MethodLog
     end
 
     it 'finds method which is defined in one commit, then removed in the next commit, and defined again in the next commit' do
-      foo_with_bar = source(path: 'foo.rb', source: %{
+      repository = Repository.new(path: repository_path)
+      commit_1 = repository.commit(source(path: 'foo.rb', source: %{
         class Foo
           def bar; end
         end
-      })
-
-      foo_without_bar = source(path: 'foo.rb', source: %{
+      }))
+      commit_2 = repository.commit(source(path: 'foo.rb', source: %{
         class Foo
         end
-      })
+      }))
+      commit_3 = repository.commit(source(path: 'foo.rb', source: %{
+        class Foo
+          def bar; end
+        end
+      }))
 
-      repository = Repository.new(path: repository_path)
-      commit_1 = repository.commit(foo_with_bar)
-      commit_2 = repository.commit(foo_without_bar)
-      commit_3 = repository.commit(foo_with_bar)
-
-      repository = Repository.new(path: repository_path)
-      api = API.new(repository: repository)
+      api = API.new(repository: Repository.new(path: repository_path))
       diffs = api.diffs('Foo#bar').map(&:last).map(&:to_s)
       expect(diffs).to eq([
         "+  def bar; end\n",
